04b37dd1567770764f90452dc8447bc585f5081e
Ciprian Turcu <ciprian.turcu@globallogic.com>
2024-01-08 10:02:13 +0200

    streamlit first push

---
+++ chat.py
@@ -0,0 +1,41 @@
+import openai
+import streamlit as st
+from lib import *
+
+# initialized variables state names and values
+session_variables = [
+    {"name": "submitted", "value": False},
+    {"name": "field_textarea_value", "value": ""},
+    {"name": "final_response", "value": ""},
+]
+
+# method to set the initialised variables to state variables
+initialise_variables(session_variables)
+
+custom_messages = initial_object_prompts()
+
+
+completion = openai.ChatCompletion.create(
+    engine="Chip-GPT4-32k",
+    messages=custom_messages,
+    temperature=0.7,
+    stream=True,
+    max_tokens=800,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None,
+)
+
+
+st.title("GPT4 Stream chat")
+st.session_state.field_textarea_value = st.text_input(
+    label="Ask a question:",
+)
+st.button(
+    label="Send" if st.session_state.submitted == False else "Clear",
+    key=4,
+    on_click=lambda: on_run_clicked(),
+)
+res_container = st.empty()
+process_response(completion)
---
+++ lib.py
@@ -0,0 +1,74 @@
+import os
+import streamlit as st
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_type = "azure"
+openai.api_base = "https://oai-playground-canadaeast-001.openai.azure.com/"
+openai.api_version = "2023-07-01-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def initialise_variables(session_variables):
+    for variable in session_variables:
+        if variable["name"] not in st.session_state:
+            # print(variable["name"], ":", variable["value"])
+            st.session_state[variable["name"]] = variable["value"]
+
+
+def on_run_clicked():
+    st.session_state.submitted = False if st.session_state.submitted == True else True
+    print(st.session_state.field_textarea_value)
+    st.session_state.final_response = ""
+    if st.session_state.submitted == False:
+        st.session_state.field_textarea_value = ""
+
+
+def initial_object_prompts():
+    custom_messages = []
+
+    custom_messages.append(
+        {
+            "role": "system",
+            "content": "You are an AI assistant that helps people find information.",
+        }
+    )
+    custom_messages.append(
+        {
+            "role": "user",
+            "content": st.session_state.field_textarea_value,
+        },
+    )
+    return custom_messages
+
+
+def process_response(response):
+    if st.session_state.submitted == True:
+        try:
+            with st.empty():
+                final_response = ""
+                for chunk in response:
+                    choices = chunk["choices"]
+                    isChoicesGreaterThanZero = len(choices) > 0
+                    if isChoicesGreaterThanZero:
+                        delta = choices[0].delta
+                        isDeltaExists = len(delta) > 0
+                        if isDeltaExists and "content" in delta:
+                            content = delta["content"]
+                            isContentExists = len(content) > 0
+                            if isContentExists:
+                                if (
+                                    isChoicesGreaterThanZero
+                                    and isDeltaExists
+                                    and isContentExists
+                                ):
+                                    final_response = final_response + content
+                                    st.write(final_response)
+                                    # st.session_state.final_response = (
+                                    #     st.session_state.final_response + content
+                                    # )
+        except Exception as e:
+            print("OpenAI Response Streaming error: " + str(e))
+            # return 503
