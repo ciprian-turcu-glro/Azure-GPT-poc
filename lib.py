5301b63d6c3dac6c6e7c71deb666ccdd749190f0
Ciprian Turcu <ciprian.turcu@globallogic.com>
2024-02-29 11:52:06 +0200

    complete code for embeddings should be refactored and tested well for functionality next

--- lib.py
+++ lib.py
@@ -2,6 +2,14 @@ import os
 import time
 import streamlit as st
 import openai
+from helper_utils import load_chroma, word_wrap, project_embeddings
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+import numpy as np
+import umap
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+import torch
 
 from dotenv import load_dotenv, find_dotenv
 
@@ -60,7 +68,7 @@ def generate_custom_text_for_simple_query_expansion(option):
         return "What was the total annual revenue for 2022?"
     elif is_option_match(option, 3):
         return "how many people do we want to recruit?"
-    elif is_option_match(option,4):
+    elif is_option_match(option, 4):
         return "what is the purpose of the Microsoft Intelligent Data 5 Platform in the context of the annual report ?"
     else:
         return ""
@@ -296,3 +304,147 @@ def apply_rag(
         retrieved_documents = results["documents"][0]
         print()
         return retrieved_documents
+
+
+def embedding_adapters(
+    file="data/2022_Annual_Report.pdf", collection_name="microsoft_annual_report_2022"
+):
+    embedding_function = SentenceTransformerEmbeddingFunction()
+
+    chroma_collection = load_chroma(
+        filename=file,
+        collection_name=collection_name,
+        embedding_function=embedding_function,
+    )
+    chroma_collection.count()
+    # embeddings
+    embeddings = chroma_collection.get(include=["embeddings"])["embeddings"]
+    umap_transform = umap.UMAP(random_state=0, transform_seed=0).fit(embeddings)
+    projected_dataset_embeddings = project_embeddings(embeddings, umap_transform)
+
+    all_generated_queries = generate_queries()
+    for query in all_generated_queries:
+        print(query)
+    query_results = chroma_collection.query(
+        query_texts=all_generated_queries,
+        n_results=10,
+        include=["documents", "embeddings"],
+    )
+    retrieved_documents = query_results["documents"]
+    retrieved_embeddings = query_results["embeddings"]
+    query_embeddings = embedding_function(all_generated_queries)
+
+    adapter_query_embeddings = []
+    adapter_doc_embeddings = []
+    adapter_labels = []
+
+    for q, query in enumerate(tqdm(all_generated_queries)):
+        for d, document in enumerate(retrieved_documents[q]):
+            adapter_query_embeddings.append(query_embeddings[q])
+            adapter_doc_embeddings.append(retrieved_embeddings[q][d])
+            adapter_labels.append(evaluate_results(query, document))
+    print(len(adapter_labels))
+    adapter_query_embeddings = torch.Tensor(np.array(adapter_query_embeddings))
+    adapter_doc_embeddings = torch.Tensor(np.array(adapter_doc_embeddings))
+    adapter_labels = torch.Tensor(np.expand_dims(np.array(adapter_labels), 1))
+
+    dataset = torch.utils.data.TensorDataset(
+        adapter_query_embeddings, adapter_doc_embeddings, adapter_labels
+    )
+
+    # Initialize the adaptor matrix
+    mat_size = len(adapter_query_embeddings[0])
+    adapter_matrix = torch.randn(mat_size, mat_size, requires_grad=True)
+
+    min_loss = float("inf")
+    best_matrix = None
+
+    for epoch in tqdm(range(100)):
+        for query_embedding, document_embedding, label in dataset:
+            loss = mse_loss(query_embedding, document_embedding, adapter_matrix, label)
+
+            if loss < min_loss:
+                min_loss = loss
+                best_matrix = adapter_matrix.clone().detach().numpy()
+
+            loss.backward()
+            with torch.no_grad():
+                adapter_matrix -= 0.01 * adapter_matrix.grad
+                adapter_matrix.grad.zero_()
+
+    print(f"Best loss: {min_loss.detach().numpy()}")
+
+    test_vector = torch.ones((mat_size, 1))
+    scaled_vector = np.matmul(best_matrix, test_vector).numpy()
+
+    plt.bar(range(len(scaled_vector)), scaled_vector.flatten())
+    plt.show()
+
+    query_embeddings = embedding_function(all_generated_queries)
+    adapted_query_embeddings = np.matmul(best_matrix, np.array(query_embeddings).T).T
+
+    projected_query_embeddings = project_embeddings(query_embeddings, umap_transform)
+    projected_adapted_query_embeddings = project_embeddings(
+        adapted_query_embeddings, umap_transform
+    )
+    # Plot the projected query and retrieved documents in the embedding space
+    plt.figure()
+    plt.scatter(projected_dataset_embeddings[:, 0], projected_dataset_embeddings[:, 1], s=10, color='gray')
+    plt.scatter(projected_query_embeddings[:, 0], projected_query_embeddings[:, 1], s=150, marker='X', color='r', label="original")
+    plt.scatter(projected_adapted_query_embeddings[:, 0], projected_adapted_query_embeddings[:, 1], s=150, marker='X', color='green', label="adapted")
+
+    plt.gca().set_aspect('equal', 'datalim')
+    plt.title("Adapted Queries")
+    plt.axis('off')
+    plt.legend()
+
+
+def generate_queries(model="Chip-GPT4-32k"):
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful expert financial research assistant. You help users analyze financial statements to better understand companies. "
+            "Suggest 10 to 15 short questions that are important to ask when analyzing an annual report. "
+            "Do not output any compound questions (questions with multiple sentences or conjunctions)."
+            "Output each question on a separate line divided by a newline.",
+        },
+    ]
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+    )
+    content = response.choices[0].message.content
+    content = content.split("\n")
+    return content
+
+
+def evaluate_results(query, statement, model="Chip-GPT4-32k"):
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful expert financial research assistant. You help users analyze financial statements to better understand companies. "
+            "For the given query, evaluate whether the following satement is relevant."
+            "Output only 'yes' or 'no'.",
+        },
+        {"role": "user", "content": f"Query: {query}, Statement: {statement}"},
+    ]
+
+    response = openai.ChatCompletion.create(
+        model=model, messages=messages, max_tokens=1
+    )
+    content = response.choices[0].message.content
+    if content == "yes":
+        return 1
+    return -1
+
+
+def model(query_embedding, document_embedding, adaptor_matrix):
+    updated_query_embedding = torch.matmul(adaptor_matrix, query_embedding)
+    return torch.cosine_similarity(updated_query_embedding, document_embedding, dim=0)
+
+
+def mse_loss(query_embedding, document_embedding, adaptor_matrix, label):
+    return torch.nn.MSELoss()(
+        model(query_embedding, document_embedding, adaptor_matrix), label
+    )
